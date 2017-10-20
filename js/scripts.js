7b2d1338918894f095ff7ee7a32f7adf5876d5e6
thyash11 <thyash11@gmail.com>
2017-10-21 00:40:53 +0530

    donate option added with ui, responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-	
+	$('.donate-box').click(function(){
+		$('.donate-page').fadeIn();
+	});
+	$(document).keyup(function(e) {
+		if (e.keyCode == 27) {
+			$('.donate-page').fadeOut();
+		}
+	});
 });
